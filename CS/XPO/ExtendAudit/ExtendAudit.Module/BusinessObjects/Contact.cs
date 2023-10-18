8ddd0cf7027f7fb87efdb379261ae52506dfaad7
andrey <andrey.kozhevnikov@devexpress.com>
2023-10-18 17:15:54 +0400

    init

---
+++ CS/XPO/ExtendAudit/ExtendAudit.Module/BusinessObjects/Contact.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.ExpressApp.Editors;
+using System.Diagnostics;
+using DevExpress.Persistent.Base.General;
+using DevExpress.ExpressApp.SystemModule;
+
+namespace dxTestSolution.Module.BusinessObjects {
+     [DefaultClassOptions]
+	  
+    public class Contact : BaseObject { 
+        public Contact(Session session)
+            : base(session) {
+        }
+        public override void AfterConstruction() {
+            base.AfterConstruction();
+        }
+        string _firstName;
+        public string FirstName {
+            get {
+                return _firstName;
+            }
+            set {
+                SetPropertyValue(nameof(FirstName), ref _firstName, value);
+            }
+        }
+        string _lastName;
+        public string LastName {
+            get {
+                return _lastName;
+            }
+            set {
+                SetPropertyValue(nameof(LastName), ref _lastName, value);
+            }
+        }
+		int _age;
+        public int Age {
+            get {
+                return _age;
+            }
+            set {
+                SetPropertyValue(nameof(Age), ref _age, value);
+            }
+        }
+  	
+        [Association("Contact-Tasks")]
+        public XPCollection<MyTask> Tasks {
+            get {
+                return GetCollection<MyTask>(nameof(Tasks));
+            }
+        }
+
+   
+
+        private BindingList<CustomAuditDataItem> auditTrail;
+        public BindingList<CustomAuditDataItem> AuditTrail {
+            get {
+                if (auditTrail == null) {
+                    auditTrail = new BindingList<CustomAuditDataItem>();
+                    auditTrail.AllowEdit = false;
+                    auditTrail.AllowNew = false;
+                    auditTrail.AllowRemove = false;
+                    XPCollection<AuditDataItemPersistent> rootItems = AuditedObjectWeakReference.GetAuditTrail(Session, this);
+                    if (rootItems != null) {
+                        foreach (AuditDataItemPersistent entry in rootItems) {
+                            auditTrail.Add(new CustomAuditDataItem(entry, "Person"));
+                        }
+                    }
+                    foreach (MyTask task in Tasks) {
+                        XPCollection<AuditDataItemPersistent> taskItems = AuditedObjectWeakReference.GetAuditTrail(Session, task);
+                        if (taskItems != null) {
+                            foreach (AuditDataItemPersistent entry in taskItems) {
+                                auditTrail.Add(new CustomAuditDataItem(entry, "Task - " + task.Oid.ToString() + ", " + task.Subject));
+                            }
+                        }
+                    }
+                }
+                return auditTrail;
+            }
+        }
+    }
+}
---
+++ CS/XPO/ExtendAudit/ExtendAudit.Module/BusinessObjects/CustomAuditDataItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.DC;
+using DevExpress.Persistent.BaseImpl;
+
+namespace dxTestSolution.Module.BusinessObjects {
+    [ DomainComponent]
+    public class CustomAuditDataItem {
+        private AuditDataItemPersistent sourceAuditDataItem;
+        private string targetObjectName;
+        public CustomAuditDataItem(AuditDataItemPersistent sourceAuditDataItem, string targetObjectName) {
+            this.sourceAuditDataItem = sourceAuditDataItem;
+            this.targetObjectName = targetObjectName;
+        }
+        public string TargetObjectName { get { return targetObjectName; } }
+        public string Description { get { return sourceAuditDataItem.Description; } }
+        public DateTime ModifiedOn { get { return sourceAuditDataItem.ModifiedOn; } }
+        public string NewValue { get { return sourceAuditDataItem.NewValue; } }
+        public string OldValue { get { return sourceAuditDataItem.OldValue; } }
+        public string OperationType { get { return sourceAuditDataItem.OperationType; } }
+        public string PropertyName { get { return sourceAuditDataItem.PropertyName; } }
+        public string UserName { get { return sourceAuditDataItem.UserName; } }
+    }
+}
